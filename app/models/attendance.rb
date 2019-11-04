2da7eb95b608e8bca9d11e00dae0d3cb06ef6b83
Fabio-Floury <floury-fabio@hotmail.fr>
2019-11-04 17:30:39 +0100

    fin

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,11 @@
 class Attendance < ApplicationRecord
 
+	after_create :event_send
+
+  def event_send
+    AttendanceMailer.attendance_email(self.user).deliver_now
+  end
+
 	belongs_to :user
   belongs_to :event
 
