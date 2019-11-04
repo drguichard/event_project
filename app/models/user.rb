b98798592b78d8f71f442bb8be6e083b8625d519
Fabio-Floury <floury-fabio@hotmail.fr>
2019-11-04 16:42:51 +0100

    leol

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
 
+	after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
 
 	has_many :appointments
   has_many :patients, through: :appointments
