0d943f06fec206fd40768e7a5973a000b2212c62
Fabio-Floury <floury-fabio@hotmail.fr>
2019-11-05 16:21:01 +0100

    ouf

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root 'static_pages#index'
+  root 'events#index'
+	get 'static_pages/index'
   get 'static_pages/secret'
+	#resources :users , only: [:show, :index]
+	resources :events
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
